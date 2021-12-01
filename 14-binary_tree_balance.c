97ba6bb887a3e78a3ddcded18e8ae0546a526c12
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-12-01 06:07:23 -0800

    fix balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,23 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left, right;
+
+    if (!tree)
+        return (0);
+    if (!tree->left && !tree->right)
+        return (0);
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
+    if (left > right)
+        return (left + 1);
+    return (right + 1);
+}
 
 /**
  * binary_tree_balance - binary_tree_balance
@@ -11,11 +30,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (0);
-	balance += (tree->left != NULL);
-	balance -= (tree->right != NULL);
-	balance += binary_tree_balance(tree->left);
-	balance -= binary_tree_balance(tree->right);
+	balance += tree->left ? (int)binary_tree_height(tree->left) : -1;
+	balance -= tree->right ? (int)binary_tree_height(tree->right) : -1; 
 	return (balance);
 }
--- main.c
+++ main.c
@@ -3,45 +3,38 @@
 #include "binary_trees.h"
 
 /**
- * launch_test - Test ancestor function and print informations
- *
- * @n1: First node
- * @n2: Second node
- */
-void launch_test(binary_tree_t *n1, binary_tree_t *n2)
-{
-    binary_tree_t *ancestor;
-
-    ancestor = binary_trees_ancestor(n1, n2);
-    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
-    if (!ancestor)
-        printf("(nil)\n");
-    else
-        printf("%d\n", ancestor->n);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    binary_tree_t *root;
+    binary_tree_t *root, *tmp;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
-    root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root->right, 128);
-    root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 45);
-    root->right->right->left = binary_tree_node(root->right->right, 92);
-    root->right->right->right = binary_tree_node(root->right->right, 65);
+    binary_tree_insert_right(root->left, 54);
+    binary_tree_insert_right(root, 128);
+    binary_tree_insert_left(root, 45);
+    binary_tree_insert_right(root->left, 50);
+    binary_tree_insert_left(root->left->left, 10);
+    binary_tree_insert_left(root->left->left->left, 8);
     binary_tree_print(root);
 
-    launch_test(root->left, root->right);
-    launch_test(root->right->left, root->right->right->right);
-    launch_test(root->right->right, root->right->right->right);
+	tmp = root;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->right;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->left;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->right->right;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->left->right;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->left->left->right;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
+	tmp = root->left->left;
+    printf("Balance of %d: %+d\n", tmp->n, binary_tree_balance(tmp));
     return (0);
 }
