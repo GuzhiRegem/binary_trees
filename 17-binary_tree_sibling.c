dea259fce62d58b388cd1467aee333d6711dfe04
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-30 16:08:01 -0800

    commit

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find sibling
+ * @node: node
+ * Return: pointer to sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *left, *right;
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+	left = node->parent->left;
+	right = node->parent->right;
+	if (left == node)
+		return (right);
+	return (left);
+}
